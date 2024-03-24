7ae3f39928232653f4a786da5f1aa7017da38314
LesegoLSG <217057620@student.uj.ac.za>
2024-03-23 20:49:03 -0500

    First commit with navbar, hero and headline sections

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Hero from './Components/Hero';
+import NavBar from './Components/NavBar';
+import HeadlineCards from './Components/HeadlineCards';
+import Food from './Components/Food';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+    </div>
+  );
+}
+
+export default App;
