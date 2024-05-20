e09169ca8bf50fae390a654f519822622b2d8b16
LesegoLSG <217057620@student.uj.ac.za>
2024-05-20 01:17:15 -0500

    Modified images

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Hero = () => {
     // Display main or landing page
   return (
-    <div className="max-w-[1640px] mx-auto p-4">
-        <div className="max-h-[500px] relative">
+    <div className="max-w-[1640px] mx-auto p-4 ">
+        <div className="max-h-[500px] relative ">
             {/* Overlay */}
             <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center">
                 <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">Your <span className="text-green-600">Cravings,</span></h1>
